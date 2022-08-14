6d3b84f370e374643b138810cb1f1241a10fd821
Ryan <48137622+rstrom1763@users.noreply.github.com>
2022-08-14 09:25:52 -0500

    Added bones of the toggle function for the browser
    The form.js file now contains a function that will be able to toggle the status of the raspi_heart from on to off or vice versa. Still requires further implementation.

--- node/static/form.js
+++ node/static/form.js
@@ -1,3 +1,25 @@
-function toggle(){
-    
+function toggle() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === request.DONE) {
+            if (request.status === 200) {
+
+            }
+            if (request.status === 404) {
+
+            }
+        }
+    }
+    try {
+        request.open("GET", 'http://' + location.host + '/getcomputer/' + data, true);
+    } catch (error) {
+        console.log(error)
+        request.open("GET", 'https://' + location.host + '/getcomputer/' + data, true);
+    }
+
+    try {
+        request.send(data.toLowerCase());
+    } catch (error) {
+        console.log(error)
+    }
 }
