9c7bbed636ba2e850351f8b362b8c8fb6c6cbcd5
Ryan <48137622+rstrom1763@users.noreply.github.com>
2022-10-21 22:06:28 -0700

    Added sanitize to client_setup
    This will be used for sanitizing the user input

--- pi_client/client_setup.py
+++ pi_client/client_setup.py
@@ -1,6 +1,27 @@
 import json
 from os import system
 
+def sanitize(data):
+    data_type = str(type(data))
+    if data_type == '<class \'int\'>':
+        return int(data)
+    data = data.lower().replace('\n','')
+    import string
+    allowed_chars = list(string.ascii_lowercase)
+    allowed_chars = list(string.ascii_uppercase)
+    for char in string.digits: allowed_chars.append(char)
+    for num in range(10): allowed_chars.append(num)
+    for char in ['-','.',' ','/']: allowed_chars.append(char)
+
+    new_string = ''
+    for char in data:
+        if char in allowed_chars:
+            new_string += char
+        else:
+            new_string += ''
+    return new_string
+
+
 config = open('./pi_client/client_config.json', 'w')
 
 server = input('Server: ').strip()
