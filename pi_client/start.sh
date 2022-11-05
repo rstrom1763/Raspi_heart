d0165ce903ca72d692144d954767e432b9250cc7
Ryan <48137622+rstrom1763@users.noreply.github.com>
2022-11-04 20:21:07 -0500

    Update start.sh

--- pi_client/start.sh
+++ pi_client/start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "Success" > /testing.txt
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-cd $(cat /var/path.txt)
-python3 /etc/PiClient.py
+
+python3 /etc/raspi_heart/PiClient.py
