353260124341308651a4ea01ba7f903289792e98
Ryan <48137622+rstrom1763@users.noreply.github.com>
2022-11-22 23:34:25 -0600

    Added proper dirs to user conf script
    The user conf script will now make sure that the ubuntu home folder exists prior to using it as well as actually cd into the directory cloned from git later on in the script. Requires testing.

--- user_config_script.sh
+++ user_config_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 curl https://get.docker.com | bash
+mkdir /home/ubuntu
 cd /home/ubuntu/
 apt install git -y
 apt install docker-compose -y
 git clone https://github.com/rstrom1763/Raspi_heart.git
+cd ./Raspi_heart
 chmod +x /home/ubuntu/Raspi_heart/start_raspi_server.sh
